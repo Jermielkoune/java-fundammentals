ddd315fb169787ed61a24d52ee737a71c3c19377
KOUNOUHO Kpessou Jermiel <kpessou-jermiel.kounouho@epitech.eu>
2026-03-27 20:03:12 +0100

    add Archer class to implement Fighter interface with attack behavior

---
+++ 01-java-core-features/src/main/java/com/sikaseal/java/fundamentals/polymorphism/Archer.java
@@ -0,0 +1,8 @@
+package com.sikaseal.java.fundamentals.polymorphism;
+
+public class Archer implements Fighter {
+    @Override
+    public String attack(){
+        return "Tire une flèche !";
+    }
+}
