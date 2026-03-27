97ea516d5a307ff307765a629f9feabbbe5a6bd8
KOUNOUHO Kpessou Jermiel <kpessou-jermiel.kounouho@epitech.eu>
2026-03-27 20:02:00 +0100

    add Knight class to implement Fighter interface with attack method

---
+++ 01-java-core-features/src/main/java/com/sikaseal/java/fundamentals/polymorphism/Knight.java
@@ -0,0 +1,8 @@
+package com.sikaseal.java.fundamentals.polymorphism;
+
+public class Knight implements Fighter {
+    @Override
+    public String attack(){
+        return "Coup d'épée !";
+    }
+}
